c3838f6435018a0a5cc93eb7a2464f3b0a04394d
ananyajain10 <jainananya355@gmail.com>
2023-10-21 19:01:37 +0530

    questions-added

---
+++ questions.js
@@ -0,0 +1,199 @@
+const questions = [
+    {   
+        
+        question: "1. A doctor and a bus driver are both in love with the same woman, an attractive girl named Sarah. The bus driver had to go on a long bus trip that would last a week. Before he left, he gave Sarah seven apples. Why?",
+        
+            a: "A. As a parting gift.",
+            b: "B. To remind her of him.",
+            c: "C. To keep the doctor away.",
+            d: "D. To make apple pie.",
+        
+        correctAnswer: "ans3"
+    },
+    {   
+        
+        question: "2. A man stands on one side of a river, his dog on the other. The man calls his dog, who immediately crosses the river without getting wet and without using a bridge or a boat. How did the dog do it?",
+        
+            a: "A. The dog can fly.",
+            b: "B. The dog used a hidden bridge.",
+            c: "C. The river was shallow.",
+            d: "D. The river was frozen",
+        
+        correctAnswer: "ans4"
+    },
+    {   
+        
+        question: "3. No matter how little or how much you use me, you change me every month. What am I?",
+        
+            a: "A. A light bulb.",
+            b: "B. A battery.",
+            c: "C. A calendar.",
+            d: "D. A clock.",
+        
+        correctAnswer: "ans3"
+    },
+    {   
+        
+        question: "4. What can be stolen, mistaken, or altered, yet never leaves you your entire life?",
+       
+            a: "A. A shadow.",
+            b: "B. A dream.",
+            c: "C. Your identity.",
+            d: "D. A memory.",
+        
+        correctAnswer: "ans3"
+    },
+    {   
+        
+        question: "5. Until I am measured, I am not known. Yet you miss me, when I have flown. What am I?",
+       
+            a: "A. A bird.",
+            b: "B. Time.",
+            c: "C. A secret.",
+            d: "D. A comet.",
+    
+        correctAnswer: "ans2"
+    },
+    {   
+        
+        question: "6.The cost of making only the maker knows, Valueless if bought, but sometimes traded. A poor man may give one as easily as a king. When one is broken pain and deceit are assured. What is it?",
+        
+            a: "A. A diamond.",
+            b: "B. A promise.",
+            c: "C. A heart.",
+            d: "D. A secret.",
+        
+        correctAnswer: "ans2"
+    },
+    {   
+        
+        question: "7. The number 8,549,176,320 is a unique number. What is so special about it?",
+        
+            a: "A. It's a prime number.",
+            b: "B. It's a palindrome.",
+            c: "C. It's the largest even number.",
+            d: "D. This is the only number that includes all the digits arranged in alphabetical order.",
+        
+            correctAnswer: "ans4"
+    },
+
+   
+];
+
+const questionImages = [
+    "ques1.jpg",
+    "./cute-husky-dog-pilot-cartoon-vector-icon-illustration-animal-transportation-icon-concept-isolated/ques2.jpg",
+    "./time-management-concept-landing-page/ques3.jpg",
+    "./dreamer-concept-illustration/ques4.jpg",
+    "./hand-drawn-flat-design-b2b-illustration/ques5.jpg",
+    "./privacy-engineering-abstract-concept-illustration/ques6.jpg",
+    "./retro-numbers-background/ques7.jpg"
+
+]
+
+
+
+
+    
+    const questionImage = document.querySelector("#ques-img");
+    const questionElement = document.querySelector("#quess");
+    const option1 = document.querySelector("#opt1");
+    const option2 = document.querySelector("#opt2");
+    const option3 = document.querySelector("#opt3");
+    const option4 = document.querySelector("#opt4");
+    const answers = document.querySelectorAll(".answer");
+        
+    const nextButton = document.querySelector("#nextButton");
+    const skipButton = document.querySelector("#skipButton");
+    const showScore = document.querySelector("#showScore");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+const questionTime = 20000;
+let countdown = questionTime;
+let timer;
+
+const loadQuestion = () => {
+
+    const quesData = questions[currentQuestionIndex];
+    questionElement.innerText = quesData.question;
+    option1.innerText = quesData.a;
+    option2.innerText = quesData.b;
+    option3.innerText = quesData.c;
+    option4.innerText = quesData.d;
+
+
+    
+}
+
+loadQuestion();
+
+
+const getCheckedAnswer= () =>{
+    let answer;
+
+    answers.forEach( (currAnsElem) => {
+        if(currAnsElem.checked){
+            answer = currAnsElem.id;
+            console.log(answer);
+            
+        } 
+    })
+    
+    return answer;
+};
+
+
+
+const deselectAll = () => {
+    answers.forEach((currAnsElem) => currAnsElem.checked = false);
+}
+
+nextButton.addEventListener("click", () => {
+    const checkedAnswer = getCheckedAnswer();
+    console.log(checkedAnswer);
+
+    if(checkedAnswer === questions[currentQuestionIndex].correctAnswer){
+        score+=5;
+       
+        
+    } else{
+        score-=1;
+        
+    }
+
+   
+    deselectAll();
+
+    if(checkedAnswer){
+        currentQuestionIndex++;
+    } else{
+        alert("Please select any option")
+    }
+
+    if(currentQuestionIndex < questions.length){
+        
+        loadQuestion();
+    } else{
+        showScore.innerHTML = `
+            <h3> You Scored ${score}/35}</h3>
+            <button class="restart-btn" onClick="location.reload()"> Play Again </button>
+        `;
+    }
+});
+
+skipButton.addEventListener("click",() => {
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        
+        loadQuestion();
+    }else{
+        showScore.innerHTML = `
+            <h3> You Scored ${score}/35}</h3>
+            <button class="restart-btn" onClick="location.reload()"> Play Again </button>
+        `;
+
+        showScore.classList.remove("showScore");
+    }
+});
---
+++ script.js
@@ -0,0 +1,22 @@
+function saveUserDetails(){
+    const fname = document.getElementById("firstname").value;
+    const lname = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+
+    if (fname && lname && email){
+        const userDetails = {
+            fname: fname,
+            lname: lname,
+            email: email
+        };
+
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
+        window.location.href = "quiz-details.html";
+    
+    } else{
+        alert("Please fill all the required data.");
+    }
+}
+
+document.getElementById("saveBtn").addEventListener("click",saveUserDetails);
